b35ffbc92c64a4370a6fb88361035dd1d42a3528
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-04-03 12:28:13 -0700

    lastest code

--- RestaurantApp/Models/RestaurantAppContext.cs
+++ RestaurantApp/Models/RestaurantAppContext.cs
@@ -8,7 +8,28 @@ namespace RestaurantApp.Models
     public DbSet<Restaurant> Restaurants { get; set; }
     public DbSet<Item> Items { get; set; }
     public DbSet<RestaurantItem> RestaurantItems { get; set; }
-    public RestaurantAppContext(DbContextOptions<RestaurantAppContext> options ) : base(options) { }
+    public RestaurantAppContext(DbContextOptions<RestaurantAppContext> options) : base (options) { }
 
-}
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      base.OnModelCreating(builder);
+      builder.Ignore<RestaurantItem>();
+      
+      builder.Entity<Restaurant>()
+        .HasData(
+          new Restaurant { RestaurantId = 1, RestaurantName = "Taco Bell", RestaurantHours = "08:00AM - 05:00PM" }
+        );
+
+      builder.Entity<Item>()
+      .HasData(
+        new Item { ItemId = 1, ItemName = "Burrito", ItemPrice = 1 }
+      );
+
+      // builder.Entity<RestaurantItem>()
+      // .HasData(
+      //   new RestaurantItem { RestaurantItemId = 1, RestaurantId = 1, ItemId = 1 }
+      // );
+
+    }
+  }
 }
